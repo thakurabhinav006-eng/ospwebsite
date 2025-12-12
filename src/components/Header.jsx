cb28be41e431a2c3483bd379503957115850d61a
Ripka <rebushirsim17@gmail.com>
2025-12-12 17:38:42 +0530

    Update hero logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
             minHeight: '60vh'
         }}>
             <div className="container">
-                <img src="/logo-v2.png" alt="OSP Services Ltd" style={{ width: '220px', marginBottom: '30px', filter: 'brightness(0) invert(1)' }} />
+                <img src="/logo-hero.png" alt="OSP Services Ltd" style={{ width: '180px', margin: '30px auto', display: 'block', objectFit: 'contain' }} />
                 <h1 style={{ fontSize: '3rem', fontWeight: '800', marginBottom: '1rem', maxWidth: '800px' }}>
                     Reliable, Affordable Cloud Services for UK & EU Small Businesses
                 </h1>
